9f6401afd14f29f224aaf46534207d02d4038796
Naghun <budad-97@hotmail.com>
2025-02-05 14:38:42 +0100

    Lighbox galery design for browser done

--- script.js
+++ script.js
@@ -19,18 +19,9 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 var imagesSwiper = new Swiper(".imagesSwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 3,
+  slidesPerView: 5,
+  spaceBetween: 30,
   loop: true,
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: false,
-  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
