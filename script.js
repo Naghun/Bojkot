63f178ea1ca796baa0bcc96495637acc86cdc5f1
Naghun <budad-97@hotmail.com>
2025-02-05 13:18:41 +0100

    fron page desings for browser done

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
     },
 });
+
+var imagesSwiper = new Swiper(".imagesSwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 3,
+  loop: true,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// Inicijalizacija GLightbox-a
+function openLightbox(img) {
+  document.getElementById("lightbox-img").src = img.src;
+  document.getElementById("lightbox").style.display = "flex";
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
